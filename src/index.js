a2afecb4aac1bfb74e615029ba45a5b08abb68d6
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2019-03-07 16:04:19 +0200

    Updated app for garie-plugin 0.0.7

--- src/index.js
+++ src/index.js
@@ -134,19 +134,32 @@ const app = express();
 app.use('/reports', express.static('reports'), serveIndex('reports', { icons: true }));
 
 const main = async () => {
-  garie_plugin.init({
-    db_name:'securityheaders',
-    getData:myGetData,
-    plugin_name:'securityheaders',
-    report_folder_name:'securityheaders-results',
-    app_root: path.join(__dirname, '..'),
-    config:config
+  return new Promise(async (resolve, reject) => {
+    try{
+      garie_plugin.init({
+        db_name:'securityheaders',
+        getData:myGetData,
+        plugin_name:'securityheaders',
+        report_folder_name:'securityheaders-results',
+        app_root: path.join(__dirname, '..'),
+        config:config
+      });
+    }
+    catch(err){
+      reject(err);
+    }
   });
 }
 
 if (process.env.ENV !== 'test') {
-  app.listen(3000, async () => {
+  const server = app.listen(3000, async () => {
     console.log('Application listening on port 3000');
-    await main();
+    try{
+      await main();
+    }
+    catch(err){
+      console.log(err);
+      server.close();
+    }
   });
 }
